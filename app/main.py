7eb200c45515d63198ad70e661bc59fc282ea0b1
Dmitry Kuleshov <vdksystem@gmail.com>
2024-04-09 21:41:49 +0200

    wrong dependency

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,6 +13,11 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name} from flux-gha-app demo project"}
+
+@app.get("/get_node")
+async def get_node():
+    node = os.environ.get("NODE_NAME", "UNKNOWN")
+    return {"message": f"Pod is running on {node}"}
 
 
 @app.get("/health")
