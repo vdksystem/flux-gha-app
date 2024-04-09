e32e4cd41a345096d1f877d4198fd31fbf7539c5
Dmitry Kuleshov <vdksystem@gmail.com>
2024-04-09 21:49:11 +0200

    src folder

--- src/main.py
+++ src/main.py
@@ -17,6 +17,7 @@ async def say_hello(name: str):
 
 @app.get("/get_node")
 async def get_node():
+    """Some docs"""
     node = os.environ.get("NODE_NAME", "UNKNOWN")
     return {"message": f"Pod is running on {node}"}
 
